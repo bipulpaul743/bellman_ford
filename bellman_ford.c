28ead7bd19e4403560223ea57ae822d700ca634d
bipulpaul743 <bipulpaul08642@gmail.com>
2025-06-20 13:27:26 +0530

    add

--- bellman_ford.c
+++ bellman_ford.c
@@ -21,47 +21,58 @@ void bellman_ford(int src) {
             int u = x[j].v1;
             int v = x[j].v2;
             int cost = x[j].wt;
-            if (dist[v] > dist[u]+cost) {
+            if (dist[u] != INT_MAX && dist[v] > dist[u]+cost) {
                 dist[v] = dist[u]+cost;
                 pred[v] = u;
             }
         }
     }
+    // Negative cycle check
     for(int j=0; j<noe; j++) {
         int u = x[j].v1;
         int v = x[j].v2;
         int cost = x[j].wt;
-        if (dist[v] > dist[u]+cost) {
-            printf("Graph contain negative weight cycle");
-            break;
+        if (dist[u] != INT_MAX && dist[v] > dist[u]+cost) {
+            printf("Graph contain negative weight cycle\n");
+            return;
         }
     }
-    for(int i=0; i<nov; i++) {
-        printf("(%d, %d)\n",dist[i], pred[i]);
+    for(int i=0; i<nov; i++){
+        printf("(%d, %c)\n", dist[i], pred[i] == -1 ? '-' : pred[i] + 65);
     }
+    // or
+    printf("or we can write,\n");
+    for(int i=0; i<nov; i++)
+        printf("For %c -> distance = %d and predecessor = %c\n",i + 65, dist[i] == INT_MAX ? -1 : dist[i], pred[i] == -1 ? '-' : pred[i] + 65);
 }
 // no need
-// void mstprint(int src){
-//     printf("mstprint\n");
-//     for(int i=0; i<nov; i++) {
-//         if(i != src) {
-//             printf("Edges(%d-%d)->%d\n",pred[i], i, dist[i]);
-//         }
-//     }
-// }
+void mstprint(int src){
+    printf("mstprint\n");
+    for(int i=0; i<nov; i++) {
+        if(i != src && pred[i] != -1) {
+            printf("Edges(%c-%c)->%d\n",pred[i]+65, i+65, dist[i]);
+        }
+    }
+}
 void adjacentprint(int src, int des)  {
-    int p[10], x, k=0;
+    int p[MAX], x, k=0;
     p[k] = des;
     x = des;
     while(x != src) {
+        if (pred[x] == -1) {
+            printf("No path from %c to %c\n", src + 65, des + 65);
+            return;
+        }
         p[++k] = pred[x];
         x = pred[x];
-        printf("%c\n",x+65);
+        //printf("%c\n",x+65);
     }
     //  for(int i=k-1; i>=0; i--) {
-    for(int i=k; i>=0; i--) {
+    printf("Path : ");
+    for(int i=k; i>0; i--) {
         printf("%c->",p[i]+65);
     }
+    printf("%c",p[0]+65);
     printf("\n");
 }
 void print_ip() {
@@ -69,17 +80,24 @@ void print_ip() {
     fp = fopen("ip_bellman_ford.txt","r");
     if(fp == NULL) {
         printf("File is empty");
+        return;
     }
     fscanf(fp, "%d",&nov);
+    int first = 1;
     for(int i=0; i<nov; i++) {
         printf("The adjacent of the vertix %c is : ",i+65);
         for(int j=0; j<nov; j++) {
             fscanf(fp, "%d",&w[i][j]);
             if(w[i][j] != 0) {
-                printf("%c, ",j+65);
+                if(!first){
+                    printf(", ");
+                }
+                printf("%c",j+65);
+                first = 0;
             }
         }
         printf("\n");
+        first = 1;
     }
     printf("Adjacency Matrix:\n");
     for(int i=0; i<nov; i++) {
@@ -95,6 +113,7 @@ void file() {
     fp = fopen("ip_bellman_ford.txt","r");
     if(fp == NULL) {
         printf("File is empty");
+        return;
     }
     fscanf(fp, "%d",&nov);
     int edgeIndex = 0;
@@ -123,12 +142,14 @@ int main() {
     printf("Enter the starting vertex : ");
     scanf("%d",&src);
     bellman_ford(src);
-    for(int i=1; i<nov; i++) {
-        printf("From %c to %c :\n",src+65,i+65);
-        adjacentprint(src, i);
-        printf("\n");
+    for(int i=0; i<nov; i++) {
+        if (i != src) {
+            printf("From %c to %c :\n",src+65,i+65);
+            adjacentprint(src, i);
+            printf("\n");
+        }
     }
-    // mstprint(src);
+    mstprint(src);
 
     return 0;
 }
